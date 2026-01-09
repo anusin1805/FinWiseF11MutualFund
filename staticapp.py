744eba32001791be853e13800b01c5624c2b0528
anusin1805 <156580126+anusin1805@users.noreply.github.com>
2026-01-09 16:48:10 +0530

    Create staticapp.py
    Signed-off-by: anusin1805 <156580126+anusin1805@users.noreply.github.com>

---
+++ staticapp.py
@@ -0,0 +1,6 @@
+app = Flask(__name__, static_folder='dist', static_url_path='/')
+
+@app.route('/', defaults={'path': ''})
+@app.route('/<path:path>')
+def serve(path):
+    return send_from_directory(app.static_folder, 'index.html')
